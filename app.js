d115a69921cb677f6f8a5e5bf02e7c9f00905826
bloodmotion <b.auger66@gmail.com>
2017-12-01 16:58:50 +0100

    first comprehension of "ze" project :)

--- app.js
+++ app.js
@@ -6,36 +6,46 @@ const NotFoundError = require('./errors/not-found')
 const app = express()
 
 const courselistRouter = require('./controllers/courselist-controller')
+const { courseList } = require('./data/db')
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 // app.get('/', (req, res, next) => {
 //   req.send('OK')
 // })
 
+// Populate DB json file
+dataBuilder = [
+    { id: 1, name: 'Toto' },
+    { id: 2, name: 'Ma liste' }
+]
+courseList.splice(0)
+courseList.push.apply(courseList, dataBuilder)
+
+
 app.use('/course-lists', courselistRouter)
 
 app.use((req, res, next) => {
-  return next(new NotFoundError())
+    return next(new NotFoundError())
 })
 
 app.use((err, req, res, next) => {
-  if (!(err instanceof HttpError)) {
-    console.error(err)
-    err = new HttpError(err.message || 'Unknown error')
-  }
+    if (!(err instanceof HttpError)) {
+        console.error(err)
+        err = new HttpError(err.message || 'Unknown error')
+    }
 
-  res.status(err.statusCode)
-  res.json({
-    error: err
-  })
+    res.status(err.statusCode)
+    res.json({
+        error: err
+    })
 })
 
 if (!module.parent) {
-  app.listen(3000, () => {
-    console.log('Server launched on port 3000')
-  })
+    app.listen(3000, () => {
+        console.log('Server launched on port 3000')
+    })
 }
 
 module.exports = app
